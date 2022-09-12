b0919849a859220d71eec88dadd89e62cea7fab4
Danila <danila@dpotapchuk.sirena-travel.ru>
2022-09-12 17:44:36 +0300

    New function of operations

--- 3_sem/calculator_js/calc.js
+++ 3_sem/calculator_js/calc.js
@@ -1,26 +1,96 @@
-function plus() {
-    let first = parseFloat(document.getElementById('first').value);
-    if (isNaN(first)) {
-        first = 0;
-        document.getElementById('first').value = first;
+// Функция сложения
+// function sum() {
+//     // Берем значения с поля ввода и вывода
+//     let output = document.getElementById('output_field').value;
+//     let input = parseFloat(document.getElementById('input_field').value);
+//     // Проверяем корректность ввода
+//     if (isNaN(input)) {
+//         input = 0
+//         document.getElementById('input_field').value = input;
+//     }
+//     // Проверка на пустоту поля вывода
+//     if (output == '') {
+//         output = input; 
+//         document.getElementById('output_field').value = output;
+//         document.getElementById('input_field').value = '';
+//     }
+//      else {
+//         // alert(parseFloat(document.getElementById('output_field').value))
+//         let sum = parseFloat(document.getElementById('output_field').value) + input;
+//         document.getElementById('output_field').value = sum;
+//         document.getElementById('input_field').value = '';
+//     }
+// }
+
+// function difference() {
+//     let output = document.getElementById('output_field').value;
+//     let input = parseFloat(document.getElementById('input_field').value);
+//     // Проверяем корректность ввода
+//     if (isNaN(input)) {
+//         input = 0
+//         document.getElementById('input_field').value = input;
+//     }
+//     // Проверка на пустоту поля вывода
+//     if (output == '') {
+//         output = input; 
+//         document.getElementById('output_field').value = output + "-";
+//         document.getElementById('input_field').value = '';
+//     } else {
+//         // alert(parseFloat(document.getElementById('output_field').value))
+//         let difference = parseFloat(document.getElementById('output_field').value) - input;
+//         document.getElementById('output_field').value = difference + "-";
+//         document.getElementById('input_field').value = '';
+//     }
+// }
+
+
+
+
+
+function operation(action) {
+    let output = document.getElementById('output_field').value;
+    let input = document.getElementById('input_field').value;
+    if (input == '') {
+        document.getElementById('output_field').value = output + action;
+    } else {
+        if (isNaN(input)) {
+            input = '';
+            alert("Я не знаю как выполнять математические операции со строками...(")
+        }
     }
-    let second = parseFloat(document.getElementById('second').value);
-    if (isNaN(second)) {
-        second = 0;
-        document.getElementById('second').value = second;
+    if (output == '') {
+        output = input;
+        document.getElementById('output_field').value = output + action;
+        document.getElementById('input_field').value = ''
+    } else { 
+        let calculation =  eval(output+input);
+        alert(calculation)
+        if (isFinite(calculation)) {
+            alert("Бесконечность не предел")
+        } else {
+            document.getElementById('output_field').value = calculation + action;
+            document.getElementById('input_field').value = '';
+        }
+        
     }
-    let sum = first + second;
-    document.getElementById("sum").value = parseFloat(sum.toFixed(9));
 }
 
+// Знак равно
 function equals() {
-    let operation = parseFloat(document.getElementById('input_field').value);
-    if (isNaN(operation)) {
-        operation = 0;
+    let input = parseFloat(document.getElementById('input_field').value);
+    if (isNaN(input)) {
+        input = 0;
         alert("Я не знаю как выполнять математические операции со строками...(")
     }
-    document.getElementById('output_field').value = operation;
+    document.getElementById('input_field').value = ''
+    let output = document.getElementById('output_field').value;
+    if (isNaN(output+input)) {
+        document.getElementById('output_field').value = eval(output + input);
+    } else {
+        alert('Выберите операцию!')
+    }
 }
+
 
 function input_numbers(id) {
     let new_number = id;
@@ -39,3 +109,4 @@ function delete_all() {
     }
     document.getElementById('input_field').value = 0; 
 }
+
