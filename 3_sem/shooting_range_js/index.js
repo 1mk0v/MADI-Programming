8bcfc6ecf96b90feb52dd9bf3884b283c6333dad
1mk0v <potapchuk01@mail.ru>
2022-12-08 03:31:32 +0300

    Logic of game

--- 3_sem/shooting_range_js/index.js
+++ 3_sem/shooting_range_js/index.js
@@ -24,20 +24,95 @@
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-class Rabbit { 
+let lastRabbitPositionX = 0;
+let lastRabbitPositionY = 0;
+let lifeOfRabbit = 3;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Rabbit {
+
+    hit() {
+        lifeOfRabbit = lifeOfRabbit -1;
+        switch (lifeOfRabbit) {
+            case 2:
+                document.getElementById('heart3').src = `${document.getElementsByTagName('script')[0].src.slice(0,-8)}photo/deadHeart.png`;
+                break;
+            case 1:
+                document.getElementById('heart2').src = `${document.getElementsByTagName('script')[0].src.slice(0,-8)}photo/deadHeart.png`;
+                break;
+            case 0:
+                document.getElementById('heart3').src = `${document.getElementsByTagName('script')[0].src.slice(0,-8)}photo/deadHeart.png`;
+                break;
+            default:
+                break;
+        }
+        return lifeOfRabbit;
+    }
+
     run() {
         let rabbit = document.getElementById('rabbit');
-        rabbit.style. = '10';
+        let newPathX = getRandomInt();
+        let newPathY = getRandomInt();
+        let newRabbitPositionX = lastRabbitPositionX + newPathX;
+        let newRabbitPositionY = lastRabbitPositionY + newPathY;
 
+        for (let i = 0; i<3; i++) {
+
+            if (newRabbitPositionX < 0) {
+                while (newRabbitPositionX < 0 || newRabbitPositionX > fieldSize()[0]) {
+                    newPathX = getRandomInt();
+                    newRabbitPositionX = lastRabbitPositionX +  newPathX;
+                }
+            }
+    
+            if (newRabbitPositionY < 0) {
+                while (newRabbitPositionY < 0 || newRabbitPositionY > fieldSize()[1]) {
+                    newPathY = getRandomInt();
+                    newRabbitPositionY = lastRabbitPositionY + newPathY;
+                }
+            }
+            rabbit.style.left = `${newRabbitPositionX + newPathX}px`;
+            rabbit.style.top = `${newRabbitPositionY + newPathX}px`;
+            newRabbitPositionX = newRabbitPositionX + newPathX;
+            newRabbitPositionY = newRabbitPositionY + newPathY;
+        }
+
+        lastRabbitPositionX = newRabbitPositionX;
+        lastRabbitPositionY = newRabbitPositionY;
+    }
+
+    isDead() {
+        if (lifeOfRabbit <= 0) {
+            return true;
+        } 
     }
 }
 
 let BugsBunny = new Rabbit;
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 function main() {
+    document.getElementById('rabbit').addEventListener('click', BugsBunny.hit);
+    GameStart();
+}
+
+function GameStart() {
+
+    if (BugsBunny.isDead()) {
+        alert('Game Over!');
+        return;
+    }
     BugsBunny.run();
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function getRandomInt() {
+    let nextPos = [-10, 0, 10];
+    return nextPos[Math.floor(Math.random() * 3)];
+} 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function mousePosition(event) {
@@ -47,11 +122,22 @@ function mousePosition(event) {
     positionY.innerHTML = event.offsetY;
 }
 
+function fieldSize() {
+    let height = document.getElementById('field').clientHeight;
+    let width = document.getElementById('field').clientWidth;
+    document.getElementById('winH').innerHTML = height;
+    document.getElementById('winW').innerHTML = width;
+    let size = [height, width];
+    return size;
+}
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function eventsListeners() {
-    document.getElementById('field').addEventListener("mousemove", mousePosition)
-    document.getElementById('start').addEventListener('click', main)
+    fieldSize();
+    window.addEventListener('resize', fieldSize);
+    document.getElementById('field').addEventListener("mousemove", mousePosition);
+    document.getElementById('start').addEventListener('click', main);
 }
 
 window.addEventListener('DOMContentLoaded', eventsListeners);
