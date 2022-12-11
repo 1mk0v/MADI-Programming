e5b8a66386b9c86bf446a16641bab7acd802d97b
1mk0v <potapchuk01@mail.ru>
2022-12-11 05:53:23 +0300

    MAIN LOGIC OF GAME

--- 3_sem/shooting_range_js/index.js
+++ 3_sem/shooting_range_js/index.js
@@ -22,19 +22,26 @@
 //                                                                                                          //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 let lastRabbitPositionX = 0;
 let lastRabbitPositionY = 0;
 let lifeOfRabbit = 3;
+let shot = 0
 
+let run;
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//ЭКЗЕМПЛЯР ПЕРСОНАЖА И ЕГО ФУНКЦИИ(ВОЗМОЖНОСТИ) 
+//МОЖНО ЗАПИСАТЬ ДАННЫЕ ФУНКЦИИ ОТДЕЛЬНО
 
 class Rabbit {
 
+    //ЕГО МОЖНО ПОДСТРЕЛИТЬ
     hit() {
         lifeOfRabbit = lifeOfRabbit -1;
-        switch (lifeOfRabbit) {
+        switch (parseInt(lifeOfRabbit)) {
             case 2:
                 document.getElementById('heart3').src = `${document.getElementsByTagName('script')[0].src.slice(0,-8)}photo/deadHeart.png`;
                 break;
@@ -50,74 +57,107 @@ class Rabbit {
         return lifeOfRabbit;
     }
 
+    //ОН УМЕЕТ БЕГАТЬ
     run() {
+
         let rabbit = document.getElementById('rabbit');
+        //Новые пути
         let newPathX = getRandomInt();
         let newPathY = getRandomInt();
+        //Следующая позиция
         let newRabbitPositionX = lastRabbitPositionX + newPathX;
         let newRabbitPositionY = lastRabbitPositionY + newPathY;
+        let time = 0;
 
-        for (let i = 0; i<3; i++) {
+        for (let i = 0; i<10; i++) {
 
-            if (newRabbitPositionX < 0) {
-                while (newRabbitPositionX < 0 || newRabbitPositionX > fieldSize()[0]) {
-                    newPathX = getRandomInt();
-                    newRabbitPositionX = lastRabbitPositionX +  newPathX;
+            setTimeout(() => {
+
+                newRabbitPositionX = lastRabbitPositionX + newPathX;
+                newRabbitPositionY = lastRabbitPositionY + newPathY;
+
+                if (newRabbitPositionX < 0 || newRabbitPositionX > fieldSize()[1] - 80) {
+                    while (newRabbitPositionX < 0 || newRabbitPositionX > fieldSize()[1] - 80) {
+                        newPathX = getRandomInt();
+                        newRabbitPositionX = lastRabbitPositionX + newPathX;
+                    }
                 }
-            }
-    
-            if (newRabbitPositionY < 0) {
-                while (newRabbitPositionY < 0 || newRabbitPositionY > fieldSize()[1]) {
-                    newPathY = getRandomInt();
-                    newRabbitPositionY = lastRabbitPositionY + newPathY;
+        
+                if (newRabbitPositionY < 0 || newRabbitPositionY > fieldSize()[0] - 80) {
+                    while (newRabbitPositionY < 0 || newRabbitPositionY > fieldSize()[0] - 80) {
+                        newPathY = getRandomInt();
+                        newRabbitPositionY = lastRabbitPositionY + newPathY;
+                    }
                 }
-            }
-            rabbit.style.left = `${newRabbitPositionX + newPathX}px`;
-            rabbit.style.top = `${newRabbitPositionY + newPathX}px`;
-            newRabbitPositionX = newRabbitPositionX + newPathX;
-            newRabbitPositionY = newRabbitPositionY + newPathY;
+
+                rabbit.style.left = `${newRabbitPositionX}px`;
+                rabbit.style.top = `${newRabbitPositionY}px`;
+
+
+                lastRabbitPositionX = newRabbitPositionX;
+                lastRabbitPositionY = newRabbitPositionY;
+                document.getElementById('rabbitPositionX').innerHTML = lastRabbitPositionX;
+                document.getElementById('rabbitPositionY').innerHTML = lastRabbitPositionY;
+            }, time);
+            time += 50;
         }
 
-        lastRabbitPositionX = newRabbitPositionX;
-        lastRabbitPositionY = newRabbitPositionY;
+        
     }
 
+    //ОН УМЕЕТ УМИРАТЬ
     isDead() {
         if (lifeOfRabbit <= 0) {
+            document.getElementById('heart3').src = `${document.getElementsByTagName('script')[0].src.slice(0,-8)}photo/deadHeart.png`;
             return true;
         } 
     }
 }
 
-let BugsBunny = new Rabbit;
+
+//СОЗДАЮ ЭКЗМЕПЛЯР ПЕРСОНАЖА
+
+let BugsBunny = new Rabbit();
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//GAME
 
 function main() {
-    document.getElementById('rabbit').addEventListener('click', BugsBunny.hit);
     GameStart();
 }
 
 function GameStart() {
-
     if (BugsBunny.isDead()) {
-        alert('Game Over!');
+        GameStop();
         return;
     }
     BugsBunny.run();
 }
 
+function GameStop() {
+    clearInterval(run);
+    document.getElementById('life').innerHTML = 'GAME OVER!';
+    document.getElementById('rabbitIMG').src = `${document.getElementsByTagName('script')[0].src.slice(0,-8)}photo/ghost.gif`;
+    document.getElementById('field').removeEventListener("click", shots);
+}
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function getRandomInt() {
-    let nextPos = [-10, 0, 10];
+    let nextPos = [-5, 0, 5];
     return nextPos[Math.floor(Math.random() * 3)];
-} 
+}
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+function shots() {
+    shot += 1;
+    document.getElementById('shotNum').innerHTML = shot;
+}
+
 function mousePosition(event) {
-    let positionX = document.getElementById('positionX');
-    let positionY = document.getElementById('positionY');
+    let positionX = document.getElementById('mousePositionX');
+    let positionY = document.getElementById('mousePositionY');
     positionX.innerHTML = event.offsetX;
     positionY.innerHTML = event.offsetY;
 }
@@ -131,13 +171,30 @@ function fieldSize() {
     return size;
 }
 
+function startRabbitPos() {
+    let height = fieldSize()[0];
+    let width = fieldSize()[1];
+    lastRabbitPositionX = width/2;
+    lastRabbitPositionY = height/2;
+    let rabbit = document.getElementById('rabbit');
+    rabbit.style.left = `${lastRabbitPositionX}px`;
+    rabbit.style.top = `${lastRabbitPositionY}px`;
+}
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function eventsListeners() {
+    startRabbitPos();
     fieldSize();
     window.addEventListener('resize', fieldSize);
     document.getElementById('field').addEventListener("mousemove", mousePosition);
-    document.getElementById('start').addEventListener('click', main);
+    document.getElementById('field').addEventListener("click", shots);
+    document.getElementById('rabbit').addEventListener('click', BugsBunny.hit);
+    // document.getElementById('start').addEventListener('click', main);
 }
 
-window.addEventListener('DOMContentLoaded', eventsListeners);
+document.addEventListener('DOMContentLoaded', eventsListeners);
+document.addEventListener('DOMContentLoaded', run = setInterval(main, 500));
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
