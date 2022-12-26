df75273b78fd4e6efd370b640a03c0ce27194cee
d.potapchuk <d.potapchuk@sirena-travel.ru>
2022-12-26 13:53:26 +0300

    Исправление ошибок

--- 3_sem/shooting_range_js/index.js
+++ 3_sem/shooting_range_js/index.js
@@ -36,6 +36,7 @@ let lifeOfRabbit = 3;
 let shot = 0;
 
 let run;
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //ЭКЗЕМПЛЯР ПЕРСОНАЖА И ЕГО ФУНКЦИИ(ВОЗМОЖНОСТИ) 
 //МОЖНО ЗАПИСАТЬ ДАННЫЕ ФУНКЦИИ ОТДЕЛЬНО
@@ -43,8 +44,8 @@ let run;
 class Rabbit {
 
     //ЕГО МОЖНО ПОДСТРЕЛИТЬ
-    hit(event) {
-        if (isReadyToHit(event)){
+    hit() {
+        if (isReadyToHit()){
             lifeOfRabbit = lifeOfRabbit -1;
             switch (parseInt(lifeOfRabbit)) {
                 case 2:
@@ -103,8 +104,6 @@ class Rabbit {
                 lastRabbitPositionX = newRabbitPositionX;
                 lastRabbitPositionY = newRabbitPositionY;
 
-                // document.getElementById('rabbitPositionX').innerHTML = lastRabbitPositionX;
-                // document.getElementById('rabbitPositionY').innerHTML = lastRabbitPositionY;
             }, time);
             time += 50;
         }        
@@ -138,7 +137,7 @@ function GameStop() {
     clearInterval(run);
     document.getElementById('field').removeEventListener("click", shots);
     document.getElementById('rabbitIMG').src = `${document.getElementsByTagName('script')[0].src.slice(0,-8)}photo/ghost.gif`;
-    document.getElementById('life').innerHTML = 'GAME OVER!\nPress F5 to restart';
+    document.getElementById('life').innerHTML = 'GAME OVER!';
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -150,44 +149,15 @@ function getRandomInt() {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //ГОТОВНОСТЬ К УДАРУ
-//поменять!
-// function isReadyToHit(event) {
-//     let pi = Math.PI;
-//     let rabbitOpt = document.getElementById('rabbit').getBoundingClientRect();
-//     let aimOpt = document.getElementById('aim').getBoundingClientRect();
-//     let maxDistans = Math.sqrt(Math.pow(rabbitOpt.width,2) + Math.pow(rabbitOpt.height))/2;
-//     let distansBetweenPoints = 0;
-//     let vectorAim = [(aimOpt.left - rabbitOpt.left), (aimOpt.top - rabbitOpt.top)];
-//     let vectorRabbit = [(rabbitOpt.right - rabbitOpt.left), (rabbitOpt.bottom - rabbitOpt.top)];
-//     let cos = (vectorRabbit[0]*vectorRabbit[1] + vectorAim[0]*vectorAim[1])/(Math.sqrt(vectorAim[0]*vectorAim[0] + vectorRabbit[0]*vectorRabbit[0]) * Math.sqrt(vectorAim[1]*vectorAim[1] + vectorRabbit[1]*vectorRabbit[1]));
-//     if (event) {
-//         // Относительно мышки
-//         // Расстояние между точками. (высчитывал по формуле длины вектора между двумя точками )
-//         distansBetweenPoints = Math.sqrt(Math.pow((lastRabbitPositionX+(rabbitOpt.width/2)-mousePositionX), 2) + Math.pow((lastRabbitPositionY+(rabbitOpt.height/2)-mousePositionY),2));
-//     } else {
-//         // Относительно картинки
-//         let aimPosX = aimOpt.left - fieldSize()[2];
-//         let aimPosY = aimOpt.top - fieldSize()[3];
-//         distansBetweenPoints = Math.sqrt(Math.pow((lastRabbitPositionX+(rabbitOpt.width/2)-aimPosX-(aimOpt.width/2)), 2) + Math.pow((lastRabbitPositionY+(rabbitOpt.height/2)-aimPosY-(aimOpt.height/2)),2)); 
-//     }
-
-//     if (distansBetweenPoints > 0 && distansBetweenPoints < maxDistans){
-//         let angleRadians = Math.acos(cos);
-//         let angleDegrees = (angleRadians*180)/pi;
-//         if (angleDegrees > 0 && angleDegrees < 90) {
-//             return true
-//         }
-//     }
-// }
 
 function isReadyToHit() {
     let rabbitOpt = document.getElementById('rabbit').getBoundingClientRect();
     let aimOpt = document.getElementById('aim').getBoundingClientRect();
     
-    if (aimOpt.left > rabbitOpt.left - rabbitOpt.width/2 &&
-        aimOpt.right < rabbitOpt.right + rabbitOpt.width/2&&
-        aimOpt.top > rabbitOpt.top - rabbitOpt.height/2&&
-        aimOpt.bottom < rabbitOpt.bottom + rabbitOpt.height/2) {
+    if (aimOpt.left > rabbitOpt.left - aimOpt.width/2 &&
+        aimOpt.right < rabbitOpt.right + aimOpt.width/2&&
+        aimOpt.top > rabbitOpt.top - aimOpt.height/2&&
+        aimOpt.bottom < rabbitOpt.bottom + aimOpt.height/2) {
             return true;
         }
 }
@@ -222,7 +192,6 @@ function aimUP() {
     if (lastAimPositionY - 8 > fieldSize()[3]) {
         lastAimPositionY = lastAimPositionY - 8;
         document.getElementById('aim').style.top = `${lastAimPositionY}px`;
-        // document.getElementById('aimPositionY').innerHTML =lastAimPositionY;
     }
 }
 
@@ -230,7 +199,6 @@ function aimLEFT() {
     if (lastAimPositionX - 8 > fieldSize()[2]) {
         lastAimPositionX = lastAimPositionX - 8;
         document.getElementById('aim').style.left = `${lastAimPositionX}px`;
-        // document.getElementById('aimPositionX').innerHTML =lastAimPositionX;
     }
 }
 
@@ -238,7 +206,6 @@ function aimRIGHT() {
     if (lastAimPositionX + 8 < fieldSize()[2]+fieldSize()[1]-80) {
         lastAimPositionX = lastAimPositionX + 8;
         document.getElementById('aim').style.left = `${lastAimPositionX}px`;
-        // document.getElementById('aimPositionX').innerHTML = lastAimPositionX;
     }
 }
 
@@ -246,7 +213,6 @@ function aimDOWN() {
     if (lastAimPositionY + 8 < fieldSize()[3]+fieldSize()[0]-80) {
         lastAimPositionY = lastAimPositionY + 8;
         document.getElementById('aim').style.top = `${lastAimPositionY}px`;
-        // document.getElementById('aimPositionY').innerHTML =lastAimPositionY;
     }
 }
 
@@ -271,8 +237,6 @@ function shots() {
 function mousePosition(event) {
     let aimOpt = document.getElementById('aim').getBoundingClientRect()
     let aim = document.getElementById('aim');
-    // let aimPosX = document.getElementById('aimPositionX');
-    // let aimPosY = document.getElementById('aimPositionY');
     //Поле срабатывания прицела
     let activeAim = 0 <= event.offsetX && event.offsetX <= fieldSize()[1] && 0 <= event.offsetY && event.offsetY <= fieldSize()[0];
     if (activeAim) {
@@ -281,8 +245,6 @@ function mousePosition(event) {
         mousePositionY = event.offsetY;
         lastAimPositionX = mousePositionX+fieldSize()[2]-aimOpt.width/2;
         lastAimPositionY = mousePositionY+fieldSize()[3]-aimOpt.height/2;
-        // aimPosX.innerHTML = lastAimPositionX-fieldSize()[2];
-        // aimPosY.innerHTML = lastAimPositionY-fieldSize()[3];
         aim.style.left = `${lastAimPositionX}px`;
         aim.style.top = `${lastAimPositionY}px`;
     }
@@ -307,8 +269,6 @@ function startRabbitPos() {
     let rabbit = document.getElementById('rabbit');
     rabbit.style.left = `${lastRabbitPositionX}px`;
     rabbit.style.top = `${lastRabbitPositionY}px`;
-    // document.getElementById('rabbitPositionX').innerHTML = lastRabbitPositionX;
-    // document.getElementById('rabbitPositionY').innerHTML = lastRabbitPositionY;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
