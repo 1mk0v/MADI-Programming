4bbe16840076ef0b70c2d4c8860b8a4b3e650762
1mk0v <potapchuk01@mail.ru>
2023-03-16 19:28:46 +0300

    new lab

---
+++ sem_4/practic_230302/index.js
@@ -0,0 +1,33 @@
+const App = {
+    data() {
+        return {
+            count: 0,
+        }
+    },
+    template: `
+    <count-reader :count='count'></count-reader>
+    <button @click="countPlus">
+        Счётчик кликов
+    </button>`,
+    methods: {
+        countPlus() {
+            this.count++;
+        }
+    }
+}
+
+
+//emit
+const app = Vue.createApp(App);
+
+app.component('count-reader', {
+    props: ['count'],
+    template: `<h4>{{countMultiplyTwo}}</h4>`,
+    computed: {
+        countMultiplyTwo() {
+            return this.count*2
+        }
+    }
+});
+
+app.mount('#my-first-components');
